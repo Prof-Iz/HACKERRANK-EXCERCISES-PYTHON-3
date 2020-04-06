449cdd87d6c8cd53eded66b5a260e6cbcb74e9f2
Prof-Iz <Prof-Iz@users.noreply.github.com>
2020-04-06 19:01:23 +0800

    split method optimised, lines removed

--- itertools-combinations.py
+++ itertools-combinations.py
@@ -36,9 +36,8 @@ HK
 '''
 from itertools import combinations
 Input = input("Enter Word followed by Max number of Combinations Ex: DOGGY 3:")
-Input = Input.split(" ") #Takes user input and splits it into list based on delimiter
-S = Input[0].upper() #first index of list is S and makes it upper case
-k = int(Input[1]) #second index of list is k, make integer as it is defaluting to string
+S, k = Input.split(" ") #Takes user input and splits it into list based on delimiter
+k = int(k) # make integer as it is defaluting to string
 newComb=[] #initialise temporary intermediate list
 final=[] #initialise final list
 if k in range(1,len(S)+1): #only does so if k is less than max and is positive int
